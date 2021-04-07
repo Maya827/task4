47d14b37820b616197cdf67540f8f07d45c67a2f
Maya827 <79314451+Maya827@users.noreply.github.com>
2021-04-07 10:48:47 +0530

    Add files via upload

---
+++ script5.js
@@ -0,0 +1,37 @@
+const getCountDown =()=>{
+    setTimeout( ()=>{
+        console.log('10');
+        setTimeout( ()=>{
+            console.log('9');
+            setTimeout( ()=>{
+                console.log('8');
+                setTimeout( ()=>{
+                    console.log('7')
+                    setTimeout( ()=>{
+                        console.log('6');
+                        setTimeout( ()=>{
+                            console.log('5');
+                            setTimeout( ()=>{
+                                console.log('4');
+                                setTimeout( ()=>{
+                                    console.log('3');
+                                    setTimeout( ()=>{
+                                        console.log('2');
+                                        setTimeout( ()=>{
+                                            console.log('1');
+                                            setTimeout( ()=>{
+                                                console.log('Happy New Year');
+                                            },1000);
+                                        },2000);
+                                    },2000);
+                                },2000);
+                            },2000);
+                        },2000);
+                    },2000);
+                },2000);
+            },2000);
+        },2000);
+        
+    },2000);
+}
+    getCountDown();
